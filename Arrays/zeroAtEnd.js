41fe733021caa4fb426f17859863aaa6bdf3c6e6
utharanambiar <utharanambiar@gmail.com>
2024-11-11 20:04:37 +0530

    remove commented code

--- Arrays/zeroAtEnd.js
+++ Arrays/zeroAtEnd.js
@@ -1,19 +1,19 @@
 let arr = [1, 0, 2, 3, 0, 4, 0, 1];
 
-// let j =0;
-// //use 2 pointers
-// for(let i=0;i<arr.length;i++) {
-//     if(arr[i] == 0) {
-//         j = i
-//         break;
-//     }
-// }
+let j = 0;
+//use 2 pointers
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] == 0) {
+    j = i;
+    break;
+  }
+}
 
-// for(let k=j + 1;k<arr.length;k++) {
-//     if(arr[k] !== 0) {
-//         [arr[k], arr[j]] =  [arr[j], arr[k]]
-//         j++;
-//     }
-// }
+for (let k = j + 1; k < arr.length; k++) {
+  if (arr[k] !== 0) {
+    [arr[k], arr[j]] = [arr[j], arr[k]];
+    j++;
+  }
+}
 
-// console.log(arr)
+console.log(arr);
