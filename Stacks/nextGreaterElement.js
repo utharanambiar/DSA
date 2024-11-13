167a037688c31807871d8f46b33519e97a6f23dc
utharanambiar <utharanambiar@gmail.com>
2024-11-13 22:57:21 +0530

    nov 12

---
+++ Stacks/nextGreaterElement.js
@@ -0,0 +1,21 @@
+let arr = [5, 7, 1, 2, 6, 0];
+//ans = [7,-1 ,2 ,6 ,7 ,5]
+let n = arr.length;
+let nge = [],
+  stck = [];
+
+for (let i = 2 * n - 1; i >= 0; i--) {
+  while (stck.length !== 0 && stck[stck.length - 1] <= arr[i % n]) {
+    stck.pop();
+  }
+  if (i < n) {
+    if (stck.length !== 0) {
+      nge[i] = stck[stck.length - 1];
+    } else {
+      nge[i] = -1;
+    }
+  }
+  stck.push(arr[i % n]);
+}
+
+console.log(nge);
