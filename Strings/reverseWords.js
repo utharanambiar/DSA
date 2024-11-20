a674b767afc7f6f1d35e25d2295665e3bf070eae
utharanambiar <utharanambiar@gmail.com>
2024-11-20 21:28:03 +0530

    reverse words

---
+++ Strings/reverseWords.js
@@ -0,0 +1,21 @@
+let str = "Hello to the world";
+let ans = [];
+let temp = "";
+for (let i = 0; i < str.length; i++) {
+  let char = str.charAt(i);
+  if (char != " ") {
+    temp = temp + char;
+  }
+  if (char == " ") {
+    ans.unshift(temp);
+    temp = "";
+  }
+}
+
+if (temp != " ") {
+  ans.unshift(temp);
+}
+
+console.log(ans.join(" "));
+//easy 1 line solution
+//console.log(str.split(" ").reverse().join(" "));
