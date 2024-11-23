8e57472e926be9ce8e29b83dec7cb09ce38f7ea7
utharanambiar <utharanambiar@gmail.com>
2024-11-23 20:23:29 +0530

    top and vertical order

---
+++ Binary Tree/verticalOrder.js
@@ -0,0 +1,63 @@
+//use map, queue for BFS traversal
+
+class Node {
+  constructor(val) {
+    this.data = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function verticalView(root) {
+  let ans = [];
+  if (root == null) {
+    return ans;
+  }
+  let newMap = new Map();
+
+  let q = [];
+
+  q.push([root, [0, 0]]);
+
+  while (q.length !== 0) {
+    let [node, [x, y]] = q.shift();
+
+    if (!newMap.has(x)) {
+      newMap.set(x, new Map());
+    }
+
+    if (!newMap.get(x).has(y)) {
+      newMap.get(x).set(y, new Set());
+    }
+
+    newMap.get(x).get(y).add(node.data);
+
+    if (node.left != null) {
+      q.push([node.left, [x - 1, y + 1]]);
+    }
+
+    if (node.right != null) {
+      q.push([node.right, [x + 1, y + 1]]);
+    }
+  }
+
+  newMap = [...newMap.entries()].sort((a, b) => a[0] - b[0]);
+  for (let [key, val] of newMap) {
+    const col = [];
+    for (const [subKey, subValue] of val) {
+      col.push(...subValue);
+    }
+    ans.push(col);
+  }
+
+  return ans;
+}
+
+const root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+
+//console.log(root);
+console.log(verticalView(root));
