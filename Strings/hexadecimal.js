eebc232e4b1af149cdf6ce4e1a571ac0b91c45e7
utharanambiar <utharanambiar@gmail.com>
2024-12-01 22:56:58 +0530

    hexadecimal conversion

---
+++ Strings/hexadecimal.js
@@ -0,0 +1,46 @@
+let num = 450.81256;
+let rem = 0,
+  q,
+  ans = [];
+let precision = 4;
+
+let map = new Map([
+  ["0", "0"],
+  ["1", "1"],
+  ["2", "2"],
+  ["3", "3"],
+  ["4", "4"],
+  ["5", "5"],
+  ["6", "6"],
+  ["7", "7"],
+  ["8", "8"],
+  ["9", "9"],
+  ["10", "A"],
+  ["11", "B"],
+  ["12", "C"],
+  ["13", "D"],
+  ["14", "E"],
+  ["15", "F"],
+]);
+
+let integerPart = Math.floor(num);
+let fractionalPart = num - integerPart;
+
+while (integerPart > 0) {
+  rem = integerPart % 16;
+  ans.unshift(map.get(rem.toString()));
+  integerPart = Math.floor(integerPart / 16);
+}
+
+if (fractionalPart > 0) {
+  ans.push(".");
+  while (precision > 0) {
+    fractionalPart *= 16;
+    let digit = Math.floor(fractionalPart);
+    ans.push(map.get(digit.toString()));
+    fractionalPart -= digit;
+    precision--;
+  }
+}
+
+console.log(ans.join(""));
