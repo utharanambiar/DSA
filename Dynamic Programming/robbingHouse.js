ce579e36816b63325bf25456f65394cec72f4d92
utharanambiar <utharanambiar@gmail.com>
2024-12-03 22:41:22 +0530

    magic index

--- Dynamic Programming/robbingHouse.js
+++ Dynamic Programming/robbingHouse.js
@@ -11,7 +11,7 @@ let maxLoot = (hval, n) => {
     secondLast = 0,
     res = 0;
 
-  for (let i = 1; i < ns; i++) {
+  for (let i = 1; i < n; i++) {
     res = Math.max(secondLast + hval[i], last);
     secondLast = last;
     last = res;
--- Strings/magicIndex.js
+++ Strings/magicIndex.js
@@ -1,3 +1,42 @@
-let arr = [-5 - 1, 2, 1, 9];
+let arr = [0, 0, 4, 5, 6];
+
+//distinct elements
+const magicIndex = (arr) => {
+  let low = 0,
+    high = arr.length - 1,
+    mid;
+
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2);
+    if (arr[mid] == mid) {
+      return true;
+    } else if (arr[mid] < mid) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return false;
+};
+
+console.log(magicIndex(arr));
 
 
+// non distinct elements
+const magicIndex2 = (arr, low, high) => {
+  if (low > high) {
+    return false; // Base case: no magic index
+  }
+  mid = Math.floor((low + high) / 2);
+  if (arr[mid] == mid) {
+    return true;
+  } 
+  
+  let leftPart = magicIndex2(arr, low, Math.min(arr[mid], mid -1))
+  if (leftPart !== -1) {
+    return leftPart
+  }
+  return magicIndex2(arr, Math.max(arr[mid], mid + 1), high);
+};
+
+console.log(magicIndex2(arr, 0, arr.length -1))
