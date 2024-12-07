57d277e06ad8d970ed8eff624fcea9211c2e67b2
utharanambiar <utharanambiar@gmail.com>
2024-12-07 22:06:01 +0530

    adjacency matrix

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -26,4 +26,44 @@ const isConnected = (node1, node2) => {
 };
 
 // console.log(findAdjacentNode("C"));
-console.log(isConnected("A", "E"))
+//console.log(isConnected("A", "E"));
+
+// using adjacency matrix
+
+let verticesIndex = {
+  A: 0,
+  B: 1,
+  C: 2,
+  D: 3,
+  E: 4,
+};
+
+let adjacencyMatrix = [
+  [0, 1, 0, 1, 0],
+  [1, 0, 1, 0, 0],
+  [0, 1, 0, 1, 1],
+  [1, 0, 1, 0, 1],
+  [0, 0, 1, 1, 0],
+];
+
+const findAdjacentNodeInMatrix = (node) => {
+  let adjacencyNode = [];
+  for (let i = 0; i < vertices.length; i++) {
+    let nodeIdx = verticesIndex[node];
+    if (adjacencyMatrix[nodeIdx][i] === 1) {
+      adjacencyNode.push(vertices[i]);
+    }
+  }
+  return adjacencyNode;
+};
+
+const isConnectedMatrix = (node1, node2) => {
+  let nodeIdx1 = verticesIndex[node1];
+  let nodeIdx2 = verticesIndex[node2];
+  if (adjacencyMatrix[nodeIdx1][nodeIdx2] === 1) {
+    return true;
+  }
+  return false;
+};
+
+console.log(isConnectedMatrix("A", "B"));
