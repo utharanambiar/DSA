d916fafd16b2858ac970bc280c7c46d7c16c4f03
utharanambiar <utharanambiar@gmail.com>
2024-12-09 23:03:30 +0530

    sum of left leaves

---
+++ Binary Tree/sumofLeftLeaves.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(val) {
+    this.data = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const isLeaf = (node) => {
+  if (node == null) {
+    return false;
+  }
+  if (node.left === null && node.right === null) {
+    return true;
+  }
+  return false;
+};
+
+const leftSum = (node) => {
+  let res = 0;
+  if (node != null) {
+    if (isLeaf(node.left)) {
+      res += node.left.data;
+    } else {
+      res += leftSum(node.left);
+      res += leftSum(node.right);
+    }
+  }
+  return res;
+};
+
+const root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.left = new Node(6);
+// ans = 4 + 6 = 10
+// right = 5
+
+console.log(leftSum(root));
