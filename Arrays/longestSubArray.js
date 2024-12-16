54d92aea749017e7c2a57ea5eef9a518b6e57cc2
utharanambiar <utharanambiar@gmail.com>
2024-12-16 20:17:32 +0530

    fix: minor update

--- Arrays/longestSubArray.js
+++ Arrays/longestSubArray.js
@@ -1,42 +1,31 @@
-let arr = [1,2,3,1,1,1,1,1,2,3]
+let arr = [1, 2, 3, 1, 1, 1, 1, 1, 2, 3];
 
 let k = 6;
 
 //sol
 
-// let left = 0, right = 0, sum = arr[0]
-// let n = arr.length, maxLen = 0
+let left = 0,
+  right = 0,
+  sum = arr[0];
+let n = arr.length,
+  maxLen = 0,
+  start = -1,
+  end = -1;
 
-// while(right < n) {
-//     while(left <= right && sum > k) {
-//         sum-= arr[left];
-//         left++
-//     }
-//     if(sum  == k) {
-//         maxLen = Math.max(maxLen, right - left + 1)
-//     }
-//     right++;
-//     sum+= arr[right]
-// }
-
-// console.log(maxLen)
-
-let arr2 = [2,6,5,8,11];
-
-let left = 0, right = arr2.length, target = 14
-
-function test() {
-while(left <= right) {
-    if(arr2[left] + arr2[right] > target) {
-        right--
-    } else if (arr2[left] + arr2[right] < target) {
-        left++
-    } else {
-        return true;
+while (right < n) {
+  while (left <= right && sum > k) {
+    sum -= arr[left];
+    left++;
+  }
+  if (sum == k) {
+    if (right - left + 1 > maxLen) {
+      maxLen = right - left + 1;
+      start = left;
+      end = right;
     }
-    return false;
-}
+  }
+  right++;
+  sum += arr[right];
 }
 
-let res = test();
-console.log(res)
+console.log(maxLen, start, end);
