26b5e4956631ac0e2c5c1d5220758096e544ebf1
utharanambiar <utharanambiar@gmail.com>
2024-12-16 22:33:29 +0530

    fix: minor update

--- Arrays/romanNumber.js
+++ Arrays/romanNumber.js
@@ -12,12 +12,11 @@ let s = "LVIII"; //op: 58 = 50 + 5 + 1 + 1 + 1
 let sum = 0;
 for (let i = 0; i < s.length; i++) {
   if (i < s.length - 1 && map.get(s[i]) < map.get(s[i + 1])) {
-    sum = sum + map.get(s[i+1]) - map.get(s[i]);
+    sum = sum + map.get(s[i + 1]) - map.get(s[i]);
     i++;
   } else {
     sum = sum + map.get(s[i]);
   }
-  console.log(sum);
 }
 
 console.log(sum);
--- Arrays/sort012.js
+++ Arrays/sort012.js
@@ -9,14 +9,12 @@ while (mid <= high) {
     [arr[low], arr[mid]] = [arr[mid], arr[low]];
     low++;
     mid++;
-  }
-  else if (arr[mid] == 1) {
+  } else if (arr[mid] == 1) {
     mid++;
-  }
-  else if (arr[mid] == 2) {
+  } else if (arr[mid] == 2) {
     [arr[high], arr[mid]] = [arr[mid], arr[high]];
     high--;
   }
 }
 
-console.log(arr)
+console.log(arr);
